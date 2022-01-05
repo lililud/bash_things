79c388f9d6a0484b21ff0197ad391b57fabc5b2e
lilian <lilian.ludford@onepeloton.com>
2022-01-05 13:45:06 -0500

    adding fish things

---
+++ conf.d/aliases.fish
@@ -0,0 +1,6 @@
+# git log
+alias gl 'git log --graph --pretty=format:"%C(dim blue)%h%C(reset) - %C(bold blue)(%ar) %C(nobold white)%s%C(reset) %C(bold white)[by %aN]%C(auto)%d%C(reset)"'
+alias gla 'git log --graph --all --pretty=format:"%C(dim blue)%h%C(reset) - %C(bold blue)(%ar) %C(nobold white)%s%C(reset) %C(bold white)[by %aN]%C(auto)%d%C(reset)"'
+alias sparkles 'echo "✨"'
+
+alias tellmesecrets 'echo "giraffe fresh seed buzz caught secret act harsh debate sing modify same"'
